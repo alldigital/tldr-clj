cfad30980b52bf5e18d5d5b936740272ec4b8080
Bluek404 <iovxw@outlook.com>
2016-01-24 18:03:07 -0600

    update to Clojure 1.8.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject tldr-clj "1.0.0"
   :description "TL;DR"
-  :url "http://iovxw.net"
-  :license {:name "MIT License"
+  :url "https://github.com/iovxw/tldr-clj"
+  :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.cli "0.3.3"]]
   :main ^:skip-aot tldr-clj.core
